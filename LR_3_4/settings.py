d0139fc6c1cd73fb05746db517723038c4cb902d
_relsa <lardeon666@gmail.com>
2022-06-15 15:54:20 +0300

    Фикс статиков

--- LR_3_4/settings.py
+++ LR_3_4/settings.py
@@ -45,6 +45,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = os.path.join(PROJECT_ROOT, 'static')
+
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
